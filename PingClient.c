d91ed3324575ffa109d76d56a89819581b38a2c6
Shreya Chati <shreyachati@cs.ucsb.edu>
2025-11-08 17:07:25 -0800

    a little woprking' '

--- PingClient.c
+++ PingClient.c
@@ -7,7 +7,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/time.h>
+#include <time.h>
 
+#define TIMEOUT_SEC 1
 
 int main(int argc, char *argv[]){
     if (argc < 3)
@@ -16,12 +19,15 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
-    struct sockaddr_in server;
-
-    //create the UDP socket:
+    // create the UDP socket:
     int socketFile = socket(AF_INET, SOCK_DGRAM, 0);
-    int length = sizeof(server);
+    struct timeval timeout;
+    timeout.tv_sec = TIMEOUT_SEC;
+    timeout.tv_usec = 0;
+    setsockopt(socketFile, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
 
+    struct sockaddr_in server;
+    int length = sizeof(server);
     //clear struct 
     bzero(&server, length);
 
@@ -32,41 +38,78 @@ int main(int argc, char *argv[]){
     server.sin_addr.s_addr = inet_addr(argv[1]);
 
     server.sin_port = htons(atoi(argv[2]));
-
-    int sequence_number = 1;
-
-    char ping_message[] = "PING";
-    printf("%s\n", ping_message);
-    
-
-    // for (int i = 0; i < 10; i++)
-    // { 
-    //     printf("%d\n", sequence_number);
-    //     sequence_number += 1;
-    // }
-
-
-    sendto(socketFile, ping_message, strlen(ping_message), 0, (const struct sockaddr *)&server, length);
     
     
-    // server_response
-    char res[1024];
-    struct sockaddr_in from;
-    socklen_t fromlen = sizeof(from);
+    int num_received = 0;
 
 
-    int message = recvfrom(socketFile, res, sizeof(res), 0, (struct sockaddr *)&from, &fromlen);
-
-    if (message < 0)
+    for (int seq_num = 1; seq_num <= 10; seq_num++)
     {
-        perror("Error receiving from server");
+        char send_msg[1024];
+        char res_buffer[1024];
+        struct timeval start_time, end_time;
+        int server_res;
+
+        
+
+        snprintf(send_msg, 1204, "PING %d %ld.%06ld",
+                 seq_num, start_time.tv_sec, (long)start_time.tv_usec);
+
+        //need to get the time before the send
+        gettimeofday(&start_time, NULL);
+        
+
+        
+        //send datagram
+        sendto(socketFile, send_msg, strlen(send_msg), 0, (const struct sockaddr *)&server, length);
+
+
+       
+        struct sockaddr_in from;
+        socklen_t fromlen = sizeof(from);
+
+        bzero(res_buffer, 1024);
+
+        server_res = recvfrom(socketFile, res_buffer, 1024 - 1, 0,
+                     (struct sockaddr *)&from, &fromlen);
+        
+        //get end time
+        gettimeofday(&end_time, NULL);
+
+
+        //recvfrom returns -1 if there was no response
+        if (server_res > 0)
+        {
+            
+            double rtt = (end_time.tv_sec - start_time.tv_sec) * 1000.0 +
+                         (end_time.tv_usec - start_time.tv_usec) / 1000.0;
+
+            //printf("Duration: %.3f ms\n", rtt);
+
+
+            res_buffer[server_res] = '\0'; 
+           
+
+            printf("PING received from %s: seq#=%d time=%.3f ms\n",
+                   argv[1], seq_num, rtt);
+
+            // Update stats
+            num_received++;
+            // rtt_sum += rtt;
+            // if (rtt_min == -1 || rtt < rtt_min)
+            //     rtt_min = rtt;
+            // if (rtt > rtt_max)
+            //     rtt_max = rtt;
+        }
+        else if (server_res < 0)
+        {
+            // Timeout
+            printf("Request timeout for: seq#=%d\n",
+                   seq_num);
+            
+
+        }
     }
-
-    // add null terminator at the end of the received bytes
-    res[message] = '\0';
-
-    // print the message from the server
-    printf("From server: %s\n", res);
 
 
     close(socketFile);
--- UDPPingerServer.py
+++ UDPPingerServer.py
@@ -11,24 +11,28 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 
 # Assign IP address and port number to socket
 # Bind on all interfaces so clients can connect via localhost or external addresses.
-serverSocket.bind(('0.0.0.0', 12000))
+serverSocket.bind(('127.0.0.1', 32000))
 
-print("UDPPingerServer listening on 0.0.0.0:12000", flush=True)
+print("UDPPingerServer listening...", flush=True)
 
 while True:
-    # Generate random number in the range of 0 to 10
-    rand = random.randint(0, 10)
+   print("\n")
+   # Generate random number in the range of 0 to 10
+   rand = random.randint(0, 10)
     
-    # Receive the client packet along with the address it is coming from
-    print("recieving")
-    message, address = serverSocket.recvfrom(1024)
-    # If rand is less is than 4, we consider the packet lost
-    #and do not respond
-    #if rand < 4:
-        #continue
+   # Receive the client packet along with the address it is coming from
+
+   message, address = serverSocket.recvfrom(1024)
+   print("received message: ", message)
+   # If rand is less is than 4, we consider the packet lost
+   #and do not respond
+    
+   if rand < 4:
+      print("rand:", rand, ", will not respond.\n")
+      continue
         
-    # Otherwise, the server responds
-    print(address)
-    print(message)
-    serverSocket.sendto(message, address)
+
+   # Otherwise, the server responds
+   serverSocket.sendto(message, address)
+   print("responded to client: ", message)
     
